f7c346e0f03ff1b5c8b296d81d7d889795c5010b
Aman Singh <103933063+youranonone10@users.noreply.github.com>
2026-04-27 16:47:11 +0530

    Modify store domain and remove unused entries
    Updated the domain for the first store and removed additional stores.

--- src/stores.js
+++ src/stores.js
@@ -9,19 +9,9 @@
 
 const STORES = [
   {
-    domain: "herohonda-2.myshopify.com",
+    domain: "x6d11a-x1.myshopify.com",
     name: "herohonda",
     webhookSecret: process.env.WEBHOOK_SECRET_STORE1,
-  },
-  {
-    domain: "your-store-2.myshopify.com",
-    name: "Store US",
-    webhookSecret: process.env.WEBHOOK_SECRET_STORE2,
-  },
-  {
-    domain: "your-store-3.myshopify.com",
-    name: "Store AE",
-    webhookSecret: process.env.WEBHOOK_SECRET_STORE3,
   },
   // ← Add more stores here following the same pattern
 ];
